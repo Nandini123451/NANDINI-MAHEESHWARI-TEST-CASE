25d71b9d75f2c35194936ce131be30a27f410885
Nandini123451 <141323443+Nandini123451@users.noreply.github.com>
2024-09-06 06:28:57 +0000

    Update Calculater.java

--- Calculater.java
+++ Calculater.java
@@ -9,7 +9,7 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("10 + 4 = " + calc.add(10, 5));
-        System.out.println("10 - 3 = " + calc.subtract(10, 5));
+        System.out.println("10 + 3 = " + calc.add(10, 5));
+        System.out.println("10 - 2 = " + calc.subtract(10, 5));
     }
 }
